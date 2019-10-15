ba8d36f1c642cd955ce75190d460f18aaf1b4e35
pawelpeksa <pmpeksa@gmail.com>
2019-10-15 10:32:13 +0200

    Update mongoCheetsheet.js

--- mongoCheetsheet.js
+++ mongoCheetsheet.js
@@ -1,6 +1,17 @@
 //Mongo cheet sheet:
 
 
+//schema:
+{
+	"_id" : BinData(0,"4a6510jZTJGcOjS5zC7etg=="),
+	"roles" : {
+		"default_user_roles" : [],
+		"all_of" : [],
+		"one_of" : []
+	},
+}
+
+
 // filter array is not empty:
 
 { "roles.one_of": { $exists: true, $not: {$size: 0}} }
@@ -15,32 +26,50 @@
 
 // Update element in array:
 
-db.clients_oauth.updateMany(
-   { "roles.one_of": "customer" },
-   { "$set": { "roles.one_of.$": "basic" } }
+db.myCollection.updateMany(
+   { "roles.one_of": "romek" },
+   { "$set": { "roles.one_of.$": "Tomek" } }
 )
 
-db.clients_oauth.updateMany(
-   { "roles.default_user_roles": "customer" },
-   { "$set": { "roles.default_user_roles.$": "basic" } }
+db.myCollection.updateMany(
+   { "roles.default_user_roles": "romek" },
+   { "$set": { "roles.default_user_roles.$": "Tomek" } }
 )
 
 
 
 //add element to array
 
-db.clients_oauth.updateMany(
+db.myCollection.updateMany(
 {},
-{ $addToSet: { 'roles.one_of': "basic" } }
+{ $addToSet: { 'roles.one_of': "Tomek" } }
 )
 
 // find when size equal
-db.clients_oauth.find(
+db.myCollection.find(
   {"roles.all_of": { $exists: true, $size: 2} }
 );
 
+
+
+
 // update when array has one particular element
-db.clients_oauth.updateMany(
-  {"roles.all_of": { $exists: true, $size: 1, $in: ["customer"]}},
+
+// all_of set to empty array if there's one element and this element is "romek"
+db.myCollection.updateMany(
+  {"roles.all_of": { $exists: true, $size: 1, $in: ["romek"]}},
   {$set: {"roles.all_of": []}}
+);
+
+
+// default_user_roles, replace romek with Tomek 
+db.myCollection.updateMany(
+   { "roles.default_user_roles": "romek" },
+   { "$set": { "roles.default_user_roles.$": "Tomek" } }
+)
+
+// one of, if romek exists add Tomek
+db.myCollection.updateMany(
+  {"roles.one_of": { $exists: true, $in: ["romek"]}},
+  { $addToSet: { 'roles.one_of':  "Tomek"  } }
 );
